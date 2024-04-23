c69408885516b19e0a63d438f0800a3e9805fa91
~ Khal!d <Khaledtg17@gmail.com>
2024-04-24 00:24:24 +0300

    Home page has been added.

--- my_kuafor_shop/lib/pages/home.dart
+++ my_kuafor_shop/lib/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -46,12 +47,185 @@ SizedBox(height: 20.0,),
 Divider(
   color: Colors.black26,
   ),
+  SizedBox(height: 30.0,),
          Text(
         "Servisler",
          style: TextStyle(
           color: Color.fromARGB(255, 0, 0, 0), 
           fontSize: 24.0, 
-          fontWeight: FontWeight.bold),),
+          fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 20.0,),
+          Row(
+            children: [
+              Flexible(
+                fit: FlexFit.tight,
+                child: Container(
+                  height: 150,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF04526F), borderRadius: BorderRadius.circular(20)
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset("images/shaving.png", height: 80, width: 80, fit: BoxFit.cover,),
+                      SizedBox(height: 10.0,),
+                       Text(
+                "Traş",
+                  style: TextStyle(
+                  color: Color(0xFFFFFFFF), 
+                  fontSize: 18.0, 
+                  fontWeight: FontWeight.bold),
+                  ),
+                
+                    ],
+                  ),
+                ),
+              ),
+              
+              SizedBox(width: 20.0,),
+
+              Flexible(
+                fit: FlexFit.tight,
+                child: Container(
+                  height: 150,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF04526F), borderRadius: BorderRadius.circular(20)
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset("images/hair.png", height: 80, width: 80, fit: BoxFit.cover,),
+                      SizedBox(height: 10.0,),
+                       Text(
+                "Saç Yakması",
+                  style: TextStyle(
+                  color: Color(0xFFFFFFFF), 
+                  fontSize: 18.0, 
+                  fontWeight: FontWeight.bold),
+                  ),
+                
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+
+           SizedBox(height: 20.0,),
+          Row(
+            children: [
+              Flexible(
+                fit: FlexFit.tight,
+                child: Container(
+                  height: 150,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF04526F), borderRadius: BorderRadius.circular(20)
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset("images/cutting.png", height: 80, width: 80, fit: BoxFit.cover,),
+                      SizedBox(height: 10.0,),
+                       Text(
+                "Saç Kesimi",
+                  style: TextStyle(
+                  color: Color(0xFFFFFFFF), 
+                  fontSize: 18.0, 
+                  fontWeight: FontWeight.bold),
+                  ),
+                
+                    ],
+                  ),
+                ),
+              ),
+              
+              SizedBox(width: 20.0,),
+
+              Flexible(
+                fit: FlexFit.tight,
+                child: Container(
+                  height: 150,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF04526F), borderRadius: BorderRadius.circular(20)
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset("images/beard.png", height: 80, width: 80, fit: BoxFit.cover,),
+                      SizedBox(height: 10.0,),
+                       Text(
+                "Sakal Kesimi",
+                  style: TextStyle(
+                  color: Color(0xFFFFFFFF), 
+                  fontSize: 18.0, 
+                  fontWeight: FontWeight.bold),
+                  ),
+                
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+
+           SizedBox(height: 20.0,),
+          Row(
+            children: [
+              Flexible(
+                fit: FlexFit.tight,
+                child: Container(
+                  height: 150,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF04526F), borderRadius: BorderRadius.circular(20)
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset("images/facials.png", height: 80, width: 80, fit: BoxFit.cover,),
+                      SizedBox(height: 10.0,),
+                       Text(
+                "Yüz Bakımı",
+                  style: TextStyle(
+                  color: Color(0xFFFFFFFF), 
+                  fontSize: 18.0, 
+                  fontWeight: FontWeight.bold),
+                  ),
+                
+                    ],
+                  ),
+                ),
+              ),
+              
+              SizedBox(width: 20.0,),
+
+              Flexible(
+                fit: FlexFit.tight,
+                child: Container(
+                  height: 150,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF04526F), borderRadius: BorderRadius.circular(20)
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset("images/kids.png", height: 80, width: 80, fit: BoxFit.cover,),
+                      SizedBox(height: 10.0,),
+                       Text(
+                "Çocuk Saç Kesimi",
+                  style: TextStyle(
+                  color: Color(0xFFFFFFFF), 
+                  fontSize: 18.0, 
+                  fontWeight: FontWeight.bold),
+                  ),
+                
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          )
+
       ],),),
     );
   }
