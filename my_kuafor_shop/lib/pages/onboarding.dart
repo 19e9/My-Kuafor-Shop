0f8e4c1913e262f9f416e7b1fc139ff6ee57123a
~ Khal!d <Khaledtg17@gmail.com>
2024-04-27 19:12:16 +0300

    Onboarding "GestureDetector" method has been added

--- my_kuafor_shop/lib/pages/onboarding.dart
+++ my_kuafor_shop/lib/pages/onboarding.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:my_kuafor_shop/pages/login.dart';
 
 class Onboarding extends StatefulWidget {
   const Onboarding({super.key});
@@ -18,18 +20,24 @@ class _OnboardingState extends State<Onboarding> {
               children: [
                 Image.asset("images/applogo.png"),
                 SizedBox(height: 10.0),
-                Container(
-                  padding:
-                      EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
-                  decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 4, 82, 111),
-                      borderRadius: BorderRadius.circular(20)),
-                  child: Text(
-                    "Siz En İyisini Hakettiğiniz İçin",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => LogIn()));
+                  },
+                  child: Container(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
+                    decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 4, 82, 111),
+                        borderRadius: BorderRadius.circular(20)),
+                    child: Text(
+                      "Siz En İyisini Hakettiğiniz İçin",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold),
+                    ),
                   ),
                 )
               ],
